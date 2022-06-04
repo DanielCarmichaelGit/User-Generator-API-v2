b7c7a52d5da4d313dfb96bdc68964245f6be2c6a
Daniel <danielfcarmichael@gmail.com>
2022-06-04 19:47:11 -0400

    duplicate endpoint deleted

--- routes/routes.js
+++ routes/routes.js
@@ -141,15 +141,6 @@ router.post('/users/createUser/batch', async(req,res) => {
     });
 })
 
-// Batch create lastNames by demographic
-router.post('/users/createLastName/batch', async(req,res) => {
-    lastNameModel.insertMany(req.body).then(function() {
-        res.status(200) // success
-    }).catch(function(error) {
-        res.status(400).json({message: error.message}) // failure
-    })
-})
-
 
 //Get all Method
 router.get('/users/getAll', async (req, res) => {
